3817e0c7cf31a449f84614a2436c67c12d2430e4
Deryan Keskin <Deryan@keskin-neubulach.de>
2022-12-11 15:10:46 +0100

    Fertig Backend mit DockerFile

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require("express"); 
 const bodyParser = require("body-parser");
 
-// const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 const v1TestRouter = require("./v1/routes/testRoutes");
 
 const app = express(); 
@@ -10,11 +9,8 @@ const API_PORT = process.env.API_PORT || 5000;
 
 app.use(bodyParser.json());
 
-// app.use("/api/v1/workouts", v1WorkoutRouter);
 app.use("/api/v1/test", v1TestRouter);
 
 app.listen(API_PORT, () => { 
     console.log(`API is listening on port ${API_PORT}`); 
-    console.log(process.env.ENV)
-    console.log(process.env.MSG)
 });
